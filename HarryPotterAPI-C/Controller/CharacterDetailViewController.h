593ce50b6982cc7c33de1d86c8f57cae043f7628
Hin Wong <hinsolo@Hins-MacBook-Pro.local>
2020-03-26 16:09:51 -0600

    storyboard complete

---
+++ HarryPotterAPI-C/Controller/CharacterDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  CharacterDetailViewController.h
+//  HarryPotterAPI-C
+//
+//  Created by Hin Wong on 3/26/20.
+//  Copyright © 2020 Hin Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CharacterDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HarryPotterAPI-C/Controller/CharacterDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  CharacterDetailViewController.m
+//  HarryPotterAPI-C
+//
+//  Created by Hin Wong on 3/26/20.
+//  Copyright © 2020 Hin Wong. All rights reserved.
+//
+
+#import "CharacterDetailViewController.h"
+
+@interface CharacterDetailViewController ()
+
+@end
+
+@implementation CharacterDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
